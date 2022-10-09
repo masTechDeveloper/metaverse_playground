cb328b2523dbf5ecb4986c3020592ff08df95184
Muhammad Ashir Ansari <ashir342@gmail.com>
2022-10-09 14:21:35 +0500

    add glb model with animations

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
-Project assets
+##Project assets
 
 https://nextjs.org/docs/getting-started
 
 https://threejs.org/
 
 https://docs.pmnd.rs/react-three-fiber/getting-started/introduction
+
+https://drei.pmnd.rs/
+
+https://www.mixamo.com/
 
 npm install three @react-three/fiber
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,54 @@
 import type { NextPage } from "next";
-
 import { Canvas } from "@react-three/fiber";
+// import CameraOrbitControler from "../components/CameraOrbitControler";
+import { OrbitControls, Stats, useAnimations } from "@react-three/drei";
+import { useEffect } from "react";
+import { useLoader } from "@react-three/fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import Lights from "../components/Lights";
+
+// import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
+
+const Character = () => {
+  // const fbx = useLoader(FBXLoader, "./models/player.fbx");
+  // return <primitive object={fbx} />;
+  const model = useLoader(GLTFLoader, "./models/player.glb");
+
+  const { actions } = useAnimations(model.animations, model.scene);
+
+  model.scene.scale.set(0.4, 0.4, 0.4);
+  // console.log(model);
+  model.scene.traverse((object) => {
+    if (object.isMesh) {
+    }
+  });
+
+  useEffect(() => {
+    actions?.idel?.play();
+  }, []);
+
+  return <primitive object={model.scene} />;
+};
 
 const Home: NextPage = () => {
   return (
     <div className="container">
-      <Canvas>
-        <ambientLight intensity={0.1} />
-        <directionalLight color="red" position={[0, 0, 5]} />
-        <mesh>
-          <boxGeometry />
-          <meshStandardMaterial />
+      <Canvas
+        camera={{ fov: 50, near: 0.1, far: 1000, position: [-2, 3, 5] }}
+        shadows
+      >
+        <OrbitControls />
+        <Stats />
+        <axesHelper args={[2]} />
+        <gridHelper />
+
+        <Lights />
+        <mesh castShadow>
+          <Character />
+        </mesh>
+        <mesh rotation-x={Math.PI * -0.5} receiveShadow>
+          <planeGeometry args={[500, 500]} />
+          <meshStandardMaterial color={"#42a7b0"} />
         </mesh>
       </Canvas>
     </div>
